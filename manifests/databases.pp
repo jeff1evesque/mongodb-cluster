0d8cdf0898d3d65531cc3f72b077febf12339876
jeff levesque <jeff1evesque@yahoo.com>
2017-03-25 18:29:50 -0400

    #3: database.pp, properly create database and users

--- manifests/databases.pp
+++ manifests/databases.pp
@@ -3,29 +3,54 @@
 ###
 
 class mongodb_cluster::databases {
+    include mongodb_cluster::install
+
     ## local variables
     $hiera_database = lookup('database')
     $hiera_mongodb  = $hiera_database['mongodb_cluster']
     $hiera_user     = $hiera_mongodb['user']
     $admin_user     = $hiera_user['admin']['name']
     $admin_password = $hiera_user['admin']['password']
+    $db_dir         = ['/data', '/data/db']
 
-    file { '/data/db':
+    file { $db_dir:
         ensure => 'directory',
         owner  => root,
         group  => root,
-        mode   => '0600',
+        mode   => '0640',
     }
 
-    mongodb::db { 'svm_dataset':
-        user          => $admin_user,
-        password_hash => $admin_password,
-        require       => File['/data/db'],
+    mongodb_database { 'svm_dataset':
+        ensure  => present,
+        tries   => 10,
+        require => [
+            Class['::mongodb::server'],
+            Class['::mongodb::client'],
+            File[$db_dir],
+        ],
     }
 
-    mongodb::db { 'svr_dataset':
-        user          => $admin_user,
-        password_hash => $admin_password,
-        require       => File['/data/db'],
+    mongodb_database { 'svr_dataset':
+        ensure  => present,
+        tries   => 10,
+        require => [
+            Class['::mongodb::server'],
+            Class['::mongodb::client'],
+            File[$db_dir],
+        ],
+    }
+
+    mongodb_user { $admin_user:
+        name          => $admin_user,
+        ensure        => present,
+        password_hash => mongodb_password($admin_user, $admin_password),
+        database      => ['svm_dataset', 'svr_dataset'],
+        roles         => ['readWrite', 'dbAdmin'],
+        tries         => 10,
+        require       => [
+            Class['::mongodb::server'],
+            Mongodb_database['svm_dataset'],
+            Mongodb_database['svr_dataset'],
+        ],
     }
 }
