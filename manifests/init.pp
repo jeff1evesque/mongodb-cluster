f7da81b2b26c065bc7d9848849664afbbacf5139
jeffrey levesque <jeff1evesque@yahoo.com>
2017-04-01 21:52:19 -0400

    #5: remove 'databases.pp' implementation

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,6 @@
 ###
 
 class mongodb_cluster {
-    $hiera_mongodb = lookup('mongodb_node')
-    $config_server = $hiera_mongodb['sharding']['clusterRole']
-
     contain mongodb_cluster::install
-    if ($config_server != 'configsvr') {
-        contain mongodb_cluster::databases
-    }
     contain mongodb_cluster::shard
 }
