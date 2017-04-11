2d48e52ced516e028be950483c6a6ab80f7653de
jeffrey levesque <jeff1evesque@yahoo.com>
2017-04-10 20:49:34 -0400

    #5: run.pp, conditionally run init scripts

--- manifests/run.pp
+++ manifests/run.pp
@@ -10,6 +10,7 @@ class mongodb_cluster::run {
     $process       = $mongodb_node['processManagement']
     $replset       = $mongodb_node['replication']
     $shard         = $mongodb_node['sharding']
+    $mongodb_type  = $mongodb_node['type']
     $engine        = $storage['engine']
     $dbPath        = $storage['dbPath']
     $journal       = $storage['journal']['enabled']
@@ -24,6 +25,7 @@ class mongodb_cluster::run {
     $pidfilepath   = $process['pidfilepath']
     $replSetName   = $replset['replSetName']
     $clusterRole   = $shard['clusterRole']
+    $configDB      = $shard['configDB']
 
     ## ensure base path
     file { $dbPath:
@@ -33,33 +35,67 @@ class mongodb_cluster::run {
         group  => root,
     }
 
-    ## general mongod configuration
-    file { '/etc/mongod.conf':
-        ensure  => file,
-        content => dos2unix(template('mongodb_cluster/mongodb.conf.erb')),
-        mode    => '0644',
-        owner   => mongodb,
-        group   => root,
-        notify  => Service['upstart-mongod'],
+    if ($mongodb_type == 'mongod') {
+        ## general mongod configuration
+        file { '/etc/mongod.conf':
+            ensure  => file,
+            content => dos2unix(template('mongodb_cluster/mongod.conf.erb')),
+            mode    => '0644',
+            owner   => mongodb,
+            group   => root,
+            notify  => Service['upstart-mongod'],
+        }
+
+        ## mongod init script
+        file { '/etc/init/upstart-mongod.conf':
+            ensure  => file,
+            content => dos2unix(template('mongodb_cluster/upstart-mongod.conf.erb')),
+            mode    => '0644',
+            owner   => mongodb,
+            group   => root,
+            notify  => Service['upstart-mongod'],
+        }
+
+        ## enforce mongod init script
+        service { 'upstart-mongod':
+            ensure  => running,
+            enable  => true,
+            require => [
+                File['/etc/mongod.conf'],
+                File['/etc/init/upstart-mongod.conf'],
+            ],
+        }
     }
 
-    ## mongod init script
-    file { '/etc/init/upstart-mongod.conf':
-        ensure  => file,
-        content => dos2unix(template('mongodb_cluster/mongod.conf.erb')),
-        mode    => '0644',
-        owner   => mongodb,
-        group   => root,
-        notify  => Service['upstart-mongod'],
-    }
+    elsif ($mongodb_type == 'mongos') {
+        ## general mongos configuration
+        file { '/etc/mongod.conf':
+            ensure  => file,
+            content => dos2unix(template('mongodb_cluster/mongos.conf.erb')),
+            mode    => '0644',
+            owner   => mongodb,
+            group   => root,
+            notify  => Service['upstart-mongos'],
+        }
 
-    ## enforce mongod init script
-    service { 'upstart-mongod':
-        ensure  => running,
-        enable  => true,
-        require => [
-            File['/etc/mongod.conf'],
-            File['/etc/init/upstart-mongod.conf'],
-        ],
+        ## mongos init script
+        file { '/etc/init/upstart-mongos.conf':
+            ensure  => file,
+            content => dos2unix(template('mongodb_cluster/upstart-mongos.conf.erb')),
+            mode    => '0644',
+            owner   => mongodb,
+            group   => root,
+            notify  => Service['upstart-mongos'],
+        }
+
+        ## enforce mongos init script
+        service { 'upstart-mongos':
+            ensure  => running,
+            enable  => true,
+            require => [
+                File['/etc/mongos.conf'],
+                File['/etc/init/upstart-mongos.conf'],
+            ],
+        }
     }
 }
